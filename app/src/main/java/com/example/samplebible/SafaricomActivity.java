34f377c19158141499d0fb62f3d6cbaba899752a
Mercy Osebe <marcynyaata@gmail.com>
2022-09-23 07:26:19 +0300

    ui finish for stk push

---
+++ app/src/main/java/com/example/samplebible/SafaricomActivity.java
@@ -0,0 +1,14 @@
+package com.example.samplebible;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SafaricomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_safaricom);
+    }
+}
